363d036af710e68118900ab5eb108667880a0a09
srihari venugopalan <sriarivenu@utexas.edu>
2017-03-05 22:39:51 -0600

    Sample

---
+++ Lab1.tar/Lab1_Spring17/AbstractProgram1.java
@@ -0,0 +1,28 @@
+public abstract class AbstractProgram1 {
+    public abstract boolean isStableMatching(Matching marriage);
+
+    /**
+     * Brute force solution to the Stable Marriage problem. Relies on the
+     * function isStableMatching(Matching) to determine whether a candidate
+     * Matching is stable.
+     * 
+     * @return A stable Matching.
+     */
+    public Matching stableMarriageBruteForce(Matching marriage) {
+        int n = marriage.getWorkerCount();
+        int m = marriage.getJobCount();
+
+        Permutation p = new Permutation(n, m);
+
+        Matching matching;
+        while ((matching = p.getNextMatching(marriage)) != null) {
+            if (isStableMatching(matching) == true) {
+                return matching;
+            }
+        }
+
+        return new Matching(marriage);
+    }
+
+    public abstract Matching stableHiringGaleShapley(Matching marriage);
+}
